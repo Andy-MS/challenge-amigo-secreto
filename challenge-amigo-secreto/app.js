ec62e2fa266928b0f6d96be1aaa17cea4c5746db
Andy-MS <andymms18@hotmail.com>
2025-03-08 23:11:40 -0600

    Implementa una función para actualizar la lista de amigos

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -13,10 +13,23 @@ function agregarAmigo (){
             alert("Por favor, inserte un nombre.");
         } else {
             amigos.push(nombreAmigo); // Agregaras el nombre al array amigos
-        }  
+            actualizarLista(); //Se llama a la función 
+            console.log("Amigo agregado: " + nombreAmigo);
+            console.log(amigos);
+            document.getElementById('amigo').value = ""; //
+        }
+}
 
-        console.log("Amigo agregado: " + nombreAmigo);
+function actualizarLista(){ //Se declara función para la lista 
+    const listaElement = document.getElementById ('listaAmigos'); //Obtener el elemento de la lista 
+    listaElement.innerHTML = ""; //Limpiar lista existente
+
+    for (let i = 0; i < amigos.length; i++){ //Iterar sobre el arreglo
+        const li = document.createElement('li');//variable que no se puede reasignar y crea elemento en la lista 
+        li.textContent = amigos[i]; // Establece el texto del elemento con el nombre del amigo
+        listaElement.appendChild(li); // Agrega el elemento a la lista
         console.log(amigos);
-        document.getElementById('amigo').value = "";
+    
+    }
 
 }
